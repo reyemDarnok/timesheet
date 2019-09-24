0d10a6e41dc000e487b0ff751a8b789edb087577
reyemDarnok <ben.k.meyer@gmail.com>
2019-09-24 19:13:42 +0200

    made rough layout, no funcionality yet
    Signed-off-by: reyemDarnok <ben.k.meyer@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Timesheet',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -44,18 +44,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,46 +54,85 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+            title: Text(widget.title),
+            actions: <Widget>[
+              new IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: _goToSettings,
+              )
+            ]
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: body()
+    ); // This trailing comma makes auto-formatting nicer for build methods
+  }
+
+  Widget body() {
+    Column main = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Überschrift"),
+          Text("Zeit heute gearbeitet"),
+          Text("Zeit heute Pause gemacht"),
+          Text("Jetzt abhauen macht minus"),
+          Text("ein/ausstempeln"),
+          Text("Pausen beginn/ende")
+        ]
+    );
+    Row r = Row(mainAxisAlignment: MainAxisAlignment.center, children: [main]);
+    return r;
+  }
+
+  _goToSettings() {
+    Navigator.of(context).push(
+        new MaterialPageRoute<void>(builder: (BuildContext) {
+          return settingsPage();
+        }));
+  }
+
+  Widget settingsPage() {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: Text("Einstellungen"),
+        ),
+        body: ListView(
+            itemExtent: 50,
+            children: <Widget>[
+              ListTile(
+                  dense: true,
+                  leading: Text("Arbeitszeit"),
+                  title: TextField(
+                    controller: new TextEditingController(
+                        text: Config.workPerDay.toString()),
+                  )
+              ),
+              ListTile(
+                  dense: true,
+                  leading: Text("Pausenzeit"),
+                  title: TextField(
+                    controller: new TextEditingController(
+                        text: Config.breakPerDay.toString()),
+                  )
+              ),
+              ListTile(
+                  dense: true,
+                  leading: Text("Ab wann wird Pause angerechnet"),
+                  title: TextField(
+                    controller: new TextEditingController(
+                        text: Config.breakCutOff.toString()),
+                  )
+              )
+            ]
+        )
     );
   }
+
+}
+
+class Config {
+  static Duration workPerDay = new Duration(hours: 7, minutes: 21);
+  static Duration breakPerDay = new Duration(hours: 1);
+  static Duration breakCutOff = new Duration(hours: 6);
 }
